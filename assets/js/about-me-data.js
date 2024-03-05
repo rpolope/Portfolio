179ca196698b0d5a198f3923e62dcf6f9bfe1cdc
Rafa <rafikax12@gmail.com>
2024-03-05 13:18:58 +0100

    correcciones para hacer que las tralsaciones vuelvan y correccion del responsive en la seccion about-me

--- assets/js/about-me-data.js
+++ assets/js/about-me-data.js
@@ -30,7 +30,7 @@ function showSection(sectionId) {
     });
     
     let sectionIdEn, sectionIdEs;
-    if (Object.keys(sectionsIds).includes(sectionId)) //El idioma actual es español
+    if (Object.keys(sectionsIds).includes(sectionId)) //Current language is Spanish
     {
         sectionIdEn = sectionsIds[sectionId];
         sectionIdEs = sectionId;
@@ -65,7 +65,7 @@ function changeContent(data, contentBox) {
         const h2 = header.querySelector('h2');
         h2.textContent = data.header;
 
-        const contentDiv = contentBox.querySelector('div');
+        const contentDiv = contentBox.querySelector('div.container');
         contentDiv.innerHTML = data.content;
         if (data.header === "Habilidades" || data.header === "Skills")
         {
@@ -109,8 +109,8 @@ const sectionsIds = {
 getContentDataFromJson();
 let aboutMeSections = Object.keys(contentData.english);
 const contentBoxes = document.querySelectorAll('#two .content.box.style2');
-let prev = document.querySelector('.nav-previous');
-let next = document.querySelector('.nav-next');
+const prevButtons = document.querySelectorAll('.nav-previous');
+const nextButtons = document.querySelectorAll('.nav-next');
 let box = document.querySelector('.slider .box');
 
 let degree = 0;
@@ -120,51 +120,54 @@ let sectionIndices = [4, 5, 0, 1, 2, 3]
 let facesIndices = [0, 1, 2, 3]
 
 // Rotation of about me section box by buttons
-prev.addEventListener('click', function(){
-    degree += 90;
-    currentCardIndex = (currentCardIndex - 1 + 4) % 4;
-    currentSectionIndex = (currentSectionIndex - 1 + aboutMeSections.length) % aboutMeSections.length;
-    box.style = `transform: perspective(1000px) rotateY(${degree}deg) translateY(100px) !important`;
+prevButtons.forEach(prev => {
+    prev.addEventListener('click', function(){
+        degree += 90;
+        currentCardIndex = (currentCardIndex - 1 + 4) % 4;
+        currentSectionIndex = (currentSectionIndex - 1 + aboutMeSections.length) % aboutMeSections.length;
+        box.style = `transform: perspective(1000px) rotateY(${degree}deg) translateY(100px) !important`;
 
-    // Both English and Spanish content divs are updated due to not synchronizing slider and displayed content
-    changeContent(contentData.english[currentSectionIndex], contentBoxes[0]);
-    changeContent(contentData.spanish[currentSectionIndex], contentBoxes[1]);
+        // Both English and Spanish content divs are updated due to not synchronizing slider and displayed content
+        changeContent(contentData.english[currentSectionIndex], contentBoxes[0]);
+        changeContent(contentData.spanish[currentSectionIndex], contentBoxes[1]);
+            
+
+        for (let i = 0; i < sectionIndices.length; i++) {
+            sectionIndices[i] = (sectionIndices[i] - 1 + sectionIndices.length) % sectionIndices.length;
+        }
+        
+        for (let i = 0; i < facesIndices.length; i++) {
+            facesIndices[i] = (facesIndices[i] - 1 + facesIndices.length) % facesIndices.length;
+        }
+        
+        let cards = document.querySelectorAll('.slider .box .card');
+        cards[facesIndices[0]].querySelector('img').src = contentData['spanish'][sectionIndices[0]].image;
+        cards[facesIndices[3]].querySelector('img').src = contentData['spanish'][sectionIndices[3]].image;
+
+    });
+});
+
+nextButtons.forEach(next => {
+    next.addEventListener('click', function(){
+        degree -= 90;
+        currentCardIndex = (currentCardIndex + 1) % 4; 
+        currentSectionIndex = (currentSectionIndex + 1) % aboutMeSections.length;
+        box.style = `transform: perspective(1000px) rotateY(${degree}deg) translateY(100px) !important`;
+
+        changeContent(contentData['english'][currentSectionIndex], contentBoxes[0]);
+        changeContent(contentData['spanish'][currentSectionIndex], contentBoxes[1]);
         
 
-    for (let i = 0; i < sectionIndices.length; i++) {
-        sectionIndices[i] = (sectionIndices[i] - 1 + sectionIndices.length) % sectionIndices.length;
-    }
-    
-    for (let i = 0; i < facesIndices.length; i++) {
-        facesIndices[i] = (facesIndices[i] - 1 + facesIndices.length) % facesIndices.length;
-    }
-    
-    let cards = document.querySelectorAll('.slider .box .card');
-    cards[facesIndices[0]].querySelector('img').src = contentData['spanish'][sectionIndices[0]].image;
-    cards[facesIndices[3]].querySelector('img').src = contentData['spanish'][sectionIndices[3]].image;
+        for (let i = 0; i < sectionIndices.length; i++) {
+            sectionIndices[i] = (sectionIndices[i] + 1) % sectionIndices.length;
+        }
+        
+        for (let i = 0; i < facesIndices.length; i++) {
+            facesIndices[i] = (facesIndices[i] + 1) % facesIndices.length;
+        }
 
+        let cards = document.querySelectorAll('.slider .box .card');
+        cards[facesIndices[0]].querySelector('img').src = contentData['spanish'][sectionIndices[0]].image;
+        cards[facesIndices[3]].querySelector('img').src = contentData['spanish'][sectionIndices[3]].image;
+    });
 });
-
-next.addEventListener('click', function(){
-    degree -= 90;
-    currentCardIndex = (currentCardIndex + 1) % 4; 
-    currentSectionIndex = (currentSectionIndex + 1) % aboutMeSections.length;
-    box.style = `transform: perspective(1000px) rotateY(${degree}deg) translateY(100px) !important`;
-
-    changeContent(contentData['english'][currentSectionIndex], contentBoxes[0]);
-    changeContent(contentData['spanish'][currentSectionIndex], contentBoxes[1]);
-    
-
-    for (let i = 0; i < sectionIndices.length; i++) {
-        sectionIndices[i] = (sectionIndices[i] + 1) % sectionIndices.length;
-    }
-    
-    for (let i = 0; i < facesIndices.length; i++) {
-        facesIndices[i] = (facesIndices[i] + 1) % facesIndices.length;
-    }
-
-    let cards = document.querySelectorAll('.slider .box .card');
-    cards[facesIndices[0]].querySelector('img').src = contentData['spanish'][sectionIndices[0]].image;
-    cards[facesIndices[3]].querySelector('img').src = contentData['spanish'][sectionIndices[3]].image;
-});
-
